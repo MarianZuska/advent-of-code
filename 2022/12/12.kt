4ee4b7ecebfe31234646334c9ce0efb2703cc75c
mz235 <marian.zuska@uni-rostock.de>
2022-12-12 20:27:29 +0100

    Refactor day 12 solution

--- 2022/12/12.kt
+++ 2022/12/12.kt
@@ -1,69 +1,71 @@
 import java.util.LinkedList
 import java.util.Queue
 
-// TODO: Refactor
-
-fun main() {
-    val input = generateSequence { readlnOrNull() }.toList()
-
-    val start = input.map { it.indexOf('S') }.withIndex().first { it.value != -1 }
-    val end = input.map { it.indexOf('E') }.withIndex().first { it.value != -1 }
-    val endPair = Pair(end.value, end.index)
-    val startPair = Pair(start.value, start.index)
-
-    val terrain = input.map { it.replace('S', 'a').replace('E', 'z') }
-
-    val possibleStarts = terrain
-        .map { it.mapIndexed { index, elem -> index.takeIf { elem == 'a' } }.filterNotNull() }
-        .withIndex()
-    val heightMap = terrain.map { line ->
-        line.map { square -> square - 'a' }
-    }.toList()
-
-    var minSteps = 1000000
-    var stepsFromS = 0
+fun bfs(start: Pair<Int, Int>, end: Pair<Int, Int>, heightMap: List<List<Int>>): Int {
     val q: Queue<Pair<Pair<Int, Int>, Int>> = LinkedList()
-    for ((startY, startXs) in possibleStarts) {
-        for (startX in startXs) {
-            val curStart = Pair(startX, startY)
-            q.clear()
-            q.add(Pair(curStart, 0))
+    q.add(Pair(start, 0))
 
-            val visited: MutableList<MutableList<Boolean>> =
-                MutableList(terrain.size) { MutableList(terrain[0].length) { false } }
+    val visited = MutableList(heightMap.size) { MutableList(heightMap[0].size) { false } }
+    while (q.isNotEmpty()) {
+        val cur = q.poll()
+        val (x, y) = cur.first
+        val steps = cur.second
 
-            while (q.isNotEmpty()) {
-                val cur = q.poll()
-                val pos = cur.first
-                val steps = cur.second
+        if (Pair(x, y) == end) return steps
 
-                if (visited[pos.second][pos.first]) continue
-                visited[pos.second][pos.first] = true
-                if (pos == endPair) {
-                    minSteps = minOf(minSteps, steps)
-                    if (curStart == startPair) stepsFromS = steps
-                    break
-                }
+        if (visited[y][x]) continue
+        visited[y][x] = true
 
-                for (dx in (-1..1)) {
-                    for (dy in (-1..1)) {
-                        if ((dx == 0 || dy == 0) && !(dx == 0 && dy == 0)) {
-                            val y = pos.second + dy
-                            val x = pos.first + dx
-                            if (x in heightMap[0].indices && y in heightMap.indices) {
-                                if (heightMap[y][x] <= heightMap[pos.second][pos.first] + 1) {
-                                    q.add(Pair(Pair(x, y), steps + 1))
-                                }
-                            }
-                        }
-                    }
+        val directions = listOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
+        for ((dx, dy) in directions) {
+            val x2 = x + dx
+            val y2 = y + dy
+            if (x2 in heightMap[0].indices && y2 in heightMap.indices) {
+                if (heightMap[y2][x2] <= heightMap[y][x] + 1) {
+                    q.add(Pair(Pair(x2, y2), steps + 1))
                 }
             }
         }
     }
+    return 1000000
+}
 
-    val out1 = stepsFromS
-    val out2 = minSteps
+fun getIndicesOfFirst(char: Char, input: List<String>): Pair<Int, Int> {
+    return input
+        .map { it.indexOf(char) }
+        .withIndex()
+        .first { it.value != -1 }
+        .run { Pair(this.value, this.index) }
+}
+
+
+fun main() {
+    val input = generateSequence { readlnOrNull() }.toList()
+
+    val startPair = getIndicesOfFirst('S', input)
+    val endPair = getIndicesOfFirst('E', input)
+
+    val terrain = input.map { it.replace('S', 'a').replace('E', 'z') }
+
+    val possibleStarts = terrain
+        .withIndex()
+        .map { (y, row) ->
+            row.indices
+                .filter { i -> row[i] == 'a' }
+                .map { Pair(it, y) }
+        }
+        .flatten()
+
+    val heightMap = terrain.map { line -> line.map { square -> square - 'a' } }.toList()
+
+    var out2 = 1000000
+    var out1 = 0
+    for (curStart in possibleStarts) {
+        val steps = bfs(curStart, endPair, heightMap)
+
+        out2 = minOf(out2, steps)
+        if (curStart == startPair) out1 = steps
+    }
 
     println(out1)
     println(out2)
