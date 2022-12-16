b09f6fd6966e3b0a4e23ad0084817b6d7e39cf5b
MarianZuska <marian.zuska@uni-rostock.de>
2022-12-16 18:07:13 +0100

    WIP change solution for day 16

--- 2022/16/16.kt
+++ 2022/16/16.kt
@@ -2,49 +2,55 @@ import java.util.*
 
 data class Valve(val name: String, val rate: Int, val adj: List<String>)
 
+data class State(
+    val position: String = "AA",
+    val time: Int = 1,
+    val openValves: Int = 0,
+    val visitedAll: Int = 0,
+    val visitedSinceLastOpen: Int = 0,
+    val flow: Int = 0
+)
+
 fun main() {
     val lines = generateSequence { readlnOrNull() }
 
     val valves: MutableMap<String, Valve> = mutableMapOf()
-
     val valveIndices: MutableMap<String, Int> = mutableMapOf()
-    val valveNames: MutableList<String> = mutableListOf()
 
-    for(line in lines) {
+    for ((i, line) in lines.withIndex()) {
         val (node, neighborString) = line.split(";")
-        val neighbors = neighborString.split(", ").map { it.substring(it.length-2) }
+        val neighbors = neighborString.split(", ").map { it.substring(it.length - 2) }
         val name = node.split(" ")[1]
         val rate = node.split("=")[1].toInt()
 
-        valveNames.add(name)
-        valveIndices[name] = valveNames.size-1
+        valveIndices[name] = i
         valves[name] = Valve(name, rate, neighbors)
     }
 
     var maxScore = 0
-    val q: Queue<Pair<Triple<String, Int, Int>, Pair<Int, Int>>> = LinkedList()
-    q.add(Pair(Triple("AA", 0, 0), Pair(1,0)))
-    while(q.isNotEmpty()) {
-        val (valveStrs, nums) = q.poll()
-        var (cur, open, visitedSinceLastOpen) = valveStrs
-        val (time, score) = nums
+    val q: Queue<State> = LinkedList()
+    q.add(State())
 
-        if(time == 30) maxScore = maxOf(maxScore, score)
-        if(time >= 30) continue
+    // TODO add monkey
+    while (q.isNotEmpty()) {
+        val (cur, time, open, visitedAll, visitedSinceLastOpen, flow) = q.poll()
 
-        valves[cur]!!.adj.forEach{ neighStr ->
-                val ind = valveIndices[neighStr]!!
-                val bitMask = (1 shl ind)
-                if(visitedSinceLastOpen and bitMask == 0) {
-                    val neigh = valves[neighStr]!!
-                    if (neigh.rate > 0 && open and bitMask == 0) {
-                        val addScore = neigh.rate * (30 - time - 1)
-                        q.add(Pair(Triple(neighStr, open + bitMask, 0), Pair(time + 2, score + addScore)))
-                    }
-                    visitedSinceLastOpen += bitMask
-                    q.add(Pair(Triple(neighStr, open, visitedSinceLastOpen), Pair(time + 1, score)))
+        if (time == 30) maxScore = maxOf(maxScore, flow)
+        if (time >= 30) continue
+
+        valves[cur]!!.adj.forEach { neigh ->
+            val ind = valveIndices[neigh]!!
+            val bitMask = (1 shl ind)
+            if (visitedSinceLastOpen and bitMask == 0) {
+                val rate = valves[neigh]!!.rate
+                if (rate > 0 && open and bitMask == 0 && visitedAll and bitMask == 0) {
+                    val addFlow = rate * (30 - time - 1)
+                    q.add(State(neigh, time + 1, open + bitMask, visitedAll + bitMask, 0, flow + addFlow))
                 }
+                q.add(State(neigh, time + 1, open, visitedAll + bitMask, visitedSinceLastOpen + bitMask, flow))
+            }
         }
+        println(time)
     }
     println(maxScore)
 }
