ee72f91ff577ae4e5c4068ed0fd03b86ebda1832
MarianZuska <marian.zuska@uni-rostock.de>
2023-12-03 21:04:54 +0100

    add day 2 rust solution

---
+++ 2023/02/02.rs
@@ -0,0 +1,38 @@
+#[allow(unused)]
+use std::io::stdin;
+use std::{self, fs};
+use std::cmp::max;
+use std::collections::HashMap;
+use regex::Regex;
+
+
+fn main() {
+    let lines = fs::read_to_string("src/in.txt").expect("failed read");
+
+    let mut sum: i32 = 0;
+    for line in lines.lines() {
+        let mut bag: HashMap<&str, i32> = HashMap::from([
+            ("red", 0), 
+            ("green", 0),
+            ("blue",0)
+        ]);
+
+        let pulls: Vec<&str> = line
+            .split(": ").nth(1).unwrap()
+            .split("; ").flat_map(|pull| pull.split(", ")).collect();
+        
+        // regex is unnecessary here but wanted to try it
+        let re: Regex = Regex::new(r"(?<amount>\d+) (?<name>\w+)").unwrap();
+        for pull in pulls {
+            let caps = re.captures(pull).expect("failed parse");
+            let name = &caps["name"];
+            let amount = caps["amount"].parse().unwrap();
+            if let Some(value) = bag.get_mut(name) { 
+                *value = max(amount, *value);
+            }
+        }
+        sum += bag.values().product::<i32>();
+    }
+    println!("{}", sum);
+
+}
